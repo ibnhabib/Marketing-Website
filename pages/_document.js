183f09422f421d4e7ea2ba75e50fb271ba3477c9
Ibnhabib <ibnhabib@users.noreply.github.com>
2023-08-29 12:26:02 +0000

    GTM installed

--- pages/_document.js
+++ pages/_document.js
@@ -14,9 +14,28 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://unpkg.com/@tailwindcss/typography@0.2.x/dist/typography.min.css"
           />
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N3CK67D');
+            `,
+          }}
+        />
        
         </Head>
         <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3CK67D"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
      
           <Main />
           <NextScript />
