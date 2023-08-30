948f4a50573529f46cea34cc876200369604ca0a
Ibnhabib <ibnhabib@users.noreply.github.com>
2023-08-30 18:50:11 +0000

    install react-gtm-module --save

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { DefaultSeo } from 'next-seo'
-
 import { SiteLayout } from '@/layout'
-
 import { defaultSEO } from '../next-seo.config'
 import { theme } from '../styles/theme'
 import '../styles/css/global.css'
 
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
+
+
+
+
 export default function App({ Component, pageProps }) {
   const getLayout =
     Component.getLayout || ((page) => <SiteLayout>{page}</SiteLayout>)
+    
+    useEffect(() => {
+      TagManager.initialize({ gtmId: "GTM-N3CK67D" })
+    }, [])
 
   return (
     <ChakraProvider theme={theme}>
